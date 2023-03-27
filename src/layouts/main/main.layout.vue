cf0a7aa4c8a6e3df25fa3aa3c2d1e390b29a61c2
unreal-zorro <unrealzorro@gmail.com>
2023-03-27 19:33:48 +0300

    main layout completed

---
+++ src/layouts/main/main.layout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="scroll">
+      <Navbar></Navbar>
+
+      <div class="container-sm main-container">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options } from "vue-class-component";
+import BaseComponent from "../../components/base/base.component";
+import Navbar from "../../components/navbar/navbar.component.vue";
+
+@Options({
+  components: { Navbar },
+  props: {}
+})
+export default class MainLayout extends BaseComponent {}
+</script>
